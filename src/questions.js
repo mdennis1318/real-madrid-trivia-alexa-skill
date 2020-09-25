3146b16f01b78b602ba39d462deee6226e0fb92d
Matt Dennis <matt@rideintandem.com>
2020-09-25 02:10:17 -0400

    init git

---
+++ src/questions.js
@@ -0,0 +1,174 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    "QUESTIONS_EN_US" : [
+        {
+            "What year was Real Madrid founded?": [
+                "1902",
+                "1910",
+                "1905",
+                "2002",
+                "1802",
+                "1886"
+            ]
+        },
+        {
+            "What year did Real Madrid start playing in their current stadium, the Santiago Bernabaeyu?": [
+                "1947",
+                "1940",
+                "1985",
+                "1936",
+                "1902"
+            ]
+        },
+        {
+            "What are Real Madrid's traditional home colors?": [
+                "White",
+                "Red",
+                "Blue",
+                "Green"
+            ]
+        },
+        {
+            "How many La Liga titles have Real Madrid won?": [
+                "33",
+                "23",
+                "31",
+                "No way to tell"
+            ]
+        },
+        {
+            "How many European Cup/UEFA Champions League trophies have Real Madrid won?": [
+                "12",
+                "10",
+                "2",
+                "15",
+                "25"
+            ]
+        },
+        {
+            "What of the following is not true?": [
+                "Christiano Ronaldo has the most appearances for the team with 725",
+                "Real Madrid won their first game at the Santiago Bernabaeyu",
+                "Madrid is the only team to win back to back Champions League trophies",
+                "The Santiago Bernabaeyu's capacity peaked at 120,000"
+            ]
+        },
+        {
+            "What company is Real Madrid's current shirt partner?": [
+                "Emirates",
+                "Qatar Airways",
+                "Vivendi",
+                "Siemens"
+            ]
+        },
+        {
+            "Who is Real Madrid's current kit manufacturer?": [
+                "Adidas",
+                "Nike",
+                "Puma",
+                "Under Armor"
+            ]
+        },
+        {
+            "What is the current capacity of the Santiago Bernabaeyu?": [
+                "81,044",
+                "90,654",
+                "99,354",
+                "100,045"
+            ]
+        },
+        {
+            "What city does Real Madrid call home?": [
+                "Madrid, Spain",
+                "London, England",
+                "Barcelona, Spain",
+                "Malaga, Spain"
+            ]
+        },
+        {
+            "What was the first year Real Madrid won the Copa del Rey?": [
+                "1905",
+                "1902",
+                "1920",
+                "1950"
+            ]
+        },
+        {
+            "Who is Real Madrid's current head coach?": [
+                "Zinedine Zidane",
+                "Florentino Perez",
+                "Jose Mourinho",
+                "Vincente del Bosque"
+            ]
+        },
+        {
+            "What year did Real Madrid first play their cross city rivals Athletico Madrid?": [
+                "1929",
+                "1900",
+                "1934",
+                "1945"
+            ]
+        },
+        {
+            "What of the following is not true?": [
+                "Real Madrid has had a crown in their logo since their founding",
+                "Real Madrid were recognized as the FIFA Club of the 20th Century",
+                "Christiano Ronaldo is the clubs top scorer of all time",
+                "The signing of Gareth Bale cost the club 100 million euros"
+            ]
+        },
+        {
+            "How many season ticket holders does Real Madrid cap sales at?": [
+                "65,000",
+                "No Cap",
+                "45,000",
+                "12,000"
+            ]
+        },
+        {
+            "What team is Real Madrid playing if the game is being called el clasico?": [
+                "F.C. Barcelona",
+                "Bayern Munich",
+                "Athletico Madrid",
+                "Malaga C.F."
+            ]
+        },
+        {
+            "What other teams other than Real Madrid have never been relegated from the spanish top flight?": [
+                "Barcelona and Athletic Bilbao",
+                "Barcelona and Athletico Madrid",
+                "Athletico Madrid and Athletic Bilbao",
+                "Malaga and Barcelona"
+            ]
+        },
+        {
+            "What would the full name of Real Madrid translate to in English?": [
+                "Royal Madrid Football Club",
+                "Football Club from Madrid",
+                "Royal Football of Spain",
+                "Madrid City Football Club"
+            ]
+        },
+        {
+            "How many years did Real Madrid win the European Cup in a row?": [
+                "5",
+                "7",
+                "3",
+                "2"
+            ]
+        },
+        {
+            "How many major trophies did Real win in the 2016-17 season?": [
+                "4",
+                "1",
+                "2",
+                "5"
+            ]
+        }
+    ]
+};
